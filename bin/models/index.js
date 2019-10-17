10133ba16ca0f38a68fb8cf4285bbf40b0eb6eb6
LVannebenne <lindsay.vannebenne@gmail.com>
2019-10-17 16:57:32 +0200

    modif models/index.js with config vars

--- bin/models/index.js
+++ bin/models/index.js
@@ -6,20 +6,15 @@ const path = require('path');
 
 const Sequelize = require('sequelize');
 
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-
-const config = require('../config/config.json')[env];
+const basename = path.basename(__filename); //const env = process.env.NODE_ENV || 'development';
+//const config = require('../config/config.json')[env];
 
 const db = {};
 let sequelize;
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
+sequelize = new Sequelize(process.env.DATABASE_DB, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  dialect: 'postgres'
+});
 fs.readdirSync(__dirname).filter(file => {
   return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
 }).forEach(file => {
--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+//const env = process.env.NODE_ENV || 'development';
+//const config = require('../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(process.env.DATABASE_DB,process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, { 
+  host: process.env.DATABASE_HOST,
+  dialect: 'postgres'
+});
+
 
 fs
   .readdirSync(__dirname)
